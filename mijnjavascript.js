8abb35953a096b61305fdbdb70cd97fbab2db512
RIckwijnhold <rick.wijnhold@student.rocva.nl>
2019-05-07 09:49:40 +0200

    Javascript DOM

---
+++ mijnjavascript.js
@@ -0,0 +1,9 @@
+var mijnauto = {
+    martk: "Ford",
+    type: "Mondeo",
+    aantalWielen: 4,
+    kleur: "blauw"
+}
+
+console.log(mijnauto.aantalWielen);
+document.getElementById("grotekop").innerHTML = mijnauto.aantalWielen;
